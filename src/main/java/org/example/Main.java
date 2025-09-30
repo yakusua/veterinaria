2f130454ae2a73d9f07a78b1aced8fae2d921c33
yakusua <lopezramirezjoancamilo@gmail.com>
2025-09-29 19:27:13 -0500

    iniciando proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,82 @@
+package org.example;
+
+
+import javax.swing.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+/*
+Ingrese una accion:
+1 - crear mascota
+2 - mostrar lista
+3 - editar mascota (con id)
+4 - eliminar mascota (con id)
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        int currentId = 0;
+        boolean loopSystem = true;
+        String vetName;
+        String vetNit;
+        vetName = JOptionPane.showInputDialog(null, "ingrese el nombre de su veterinaria");
+        vetNit = JOptionPane.showInputDialog(null, "ingrese el nit");
+        Veterinaria veterinaria = new Veterinaria(vetNit,vetName);
+        while (loopSystem) {
+            String optionVet = JOptionPane.showInputDialog(
+                    null,
+                    "Ingrese una accion:\n" +
+                            "1 - crear mascota\n" +
+                            "2 - mostrar lista\n" +
+                            "3 - editar mascota (con id)\n" +
+                            "4 - eliminar mascota (con id)\n" +
+                            "5 - close"
+            );
+            switch (Integer.parseInt(optionVet)) {
+                case 1:
+                    currentId++;
+                    String nombre = JOptionPane.showInputDialog(null, "ingrese el nombre");
+                    String especie = JOptionPane.showInputDialog(null, "ingrese la especie");
+                    String raza = JOptionPane.showInputDialog(null, "ingrese la raza");
+                    String edad = JOptionPane.showInputDialog(null, "ingrese la edad (solo numeros)");
+                    String nombrePropietario = JOptionPane.showInputDialog(null, "ingrese el  nombre del propietario");
+                    String numPropietario = JOptionPane.showInputDialog(null, "ingrese el numero del propietario");
+                    veterinaria.createMascota(
+                            String.valueOf(currentId),
+                            nombre,
+                            especie,
+                            raza,
+                            Integer.parseInt(edad),
+                            nombrePropietario,
+                            numPropietario
+                    );
+                    break;
+                case 2:
+                    String listaMascotas =veterinaria.getListMascotas();
+                    JOptionPane.showMessageDialog(null, listaMascotas);
+                    break;
+                case 3:
+                    currentId++;
+                    String id = JOptionPane.showInputDialog(null,"ingrese el id de la mascota a editar");
+                    String nombreEdit = JOptionPane.showInputDialog(null, "ingrese el nombre");
+                    String especieEdit = JOptionPane.showInputDialog(null, "ingrese la especie");
+                    String razaEdit = JOptionPane.showInputDialog(null, "ingrese la raza");
+                    String edadEdit = JOptionPane.showInputDialog(null, "ingrese la edad");
+                    String nombrePropietarioEdit = JOptionPane.showInputDialog(null, "ingrese el  nombre del propietario");
+                    String numPropietarioEdit = JOptionPane.showInputDialog(null, "ingrese el numero del propietario");
+                    veterinaria.UpdateMascota(id,nombreEdit,especieEdit,razaEdit, Integer.parseInt(edadEdit),numPropietarioEdit,nombrePropietarioEdit);
+                    break;
+                case 4:
+                    String idDelete = JOptionPane.showInputDialog(null,"ingrese el id de la mascota a editar");
+                    veterinaria.DeleteMascota(idDelete);
+                    break;
+                case 5:
+                    loopSystem = false;
+            }
+        }
+
+    }
+
+}
---
+++ src/main/java/org/example/Mascota.java
@@ -0,0 +1,99 @@
+package org.example;
+/*
+ id,
+ nombre,
+ especie,
+ raza,
+ edad,
+ nombrePropietario
+ numPropietario.
+ */
+public class Mascota {
+    private String id;
+    private String nombre;
+    private String especie;
+    private String raza;
+    private int edad;
+    private String numPropietario;
+    private String nombrePropietario;
+
+    public Mascota(
+            String id,
+            String nombre,
+            String especie,
+            String raza,
+            int edad,
+            String numPropietario,
+            String nombrePropietario
+    ) {
+        this.id = id;
+        this.nombre = nombre;
+        this.especie = especie;
+        this.raza = raza;
+        this.edad = edad;
+        this.numPropietario = numPropietario;
+        this.nombrePropietario = nombrePropietario;
+    }
+
+    // get set id
+    public String getId(){
+        return id;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+
+    // get set nombre
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    // get set puesto
+    public String getEspecie(){
+        return especie;
+    }
+    public void setEspecie(String especie){
+        this.especie = especie;
+    }
+
+    // get set correo
+    public String getRaza(){
+        return raza;
+    }
+    public void setRaza(String raza){
+        this.raza = raza;
+    }
+    // get set edad
+    public int getEdad(){
+        return edad;
+    }
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+
+    // get set numPropietario
+    public String getNumPropietario(){
+        return numPropietario;
+    }
+    public void setNumPropietario(String numTelefonico){
+        this.numPropietario = numTelefonico;
+    }
+
+    // get set fechaContratacion
+    public String getNombrePropietario(){
+        return nombrePropietario;
+    }
+    public void setNombrePropietario(String fechaContratacion){
+        this.nombrePropietario = fechaContratacion;
+    }
+
+    // Override toString
+    @Override
+    public String toString(){
+        return "id: " +id+ "  nombre: "+nombre +"  edad: "+edad + "  nombrePropietario: "+nombrePropietario + "  numPropietario: "+numPropietario ;
+    }
+
+}
